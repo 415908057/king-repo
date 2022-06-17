50231ab1c62e5f616fcf697495598acb59faf18d
Your Name <email@example.com>
2022-06-17 22:58:42 +0800

    hello

---
+++ java/src/Test/Hello.java
@@ -0,0 +1,13 @@
+package Test;
+
+/**
+ * @Auther: King
+ * @Date: 2022/6/17 - 06 - 17 - 22:57
+ * @Description: Test
+ * @version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
