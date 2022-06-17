c2107e2eb1a91947396725cb3e4695072758fb3e
415908057 <107624124+415908057@users.noreply.github.com>
2022-06-17 23:38:01 +0800

    Update DemoApplication.java

--- java/src/main/java/com/example/demo/DemoApplication.java
+++ java/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+     public static void get(){
+     log.info("hello");
+     }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
